d6e396fd086bd2d281a7216c2a71ebe75dabe8f3
Elise S <sunderla17@up.edu>
2017-08-15 13:51:23 -0700

    redid components to be more reliable, added Bootstrap styles

--- src/components/AddressForm.js
+++ src/components/AddressForm.js
@@ -1,40 +1,74 @@
 import React, {Component} from "react";
-import {TextFieldValidated} from 'lucid-ui';
-import '../../node_modules/lucid-ui/dist/index.css';
+import {TextFieldValidated, Button} from 'lucid-ui';
+import Form from 'react-jsonschema-form';
+import '../styles/css/bootstrap.min.css';
 
+//google maps/timezone/geocoding api key AIzaSyClW8MQo9G707kp0lwc5Q3YlvVNg66jR1c
+const schema = {
+    title: "",
+    type: "object",
+    required: ["title", "city", "state", "zip"],
+    properties: {
+        title: {
+            type: "string",
+            title: "Building # and Street",
+            default: "",
+        },
+        city: {
+            type: "string",
+            title: "City",
+            default: "",
+        },
+        state: {
+            type: "string",
+            title: "State",
+            default: "",
+        },
+        zip: {
+            type: "number",
+            title: "Zip Code",
+            default: "",
+        }
+    }
+}
+
+const uiSchema = {
+    title: {
+        "ui:placeholder": "1234 NW Main St.",
+    },
+    city: {
+        "ui:placeholder": "New York City",
+    },
+    state: {
+        "ui:placeholder": "NY",
+    },
+    zip: {
+        "ui:placeholder": "10019",
+    }
+}
+
+//function to submit address forms as stringified json
+function collectInput () {
+
+}
 //This component is a validated text field for US address formats
-const AddressForm = React.createClass ({
-    getInitialState() {
-		return {
-			value: '',
-		};
-	},
+class AddressForm extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            value: '',
+        };
+    }
     render() {
         return (
             <div id="addressGroup">
-                <h3> Address </h3>
-                <TextFieldValidated
-                    value={this.state.value}
-                    Error={this.state.value === 'Address' ? null : 'Please enter an address in the following format: Building # Street Name'}
-                />
-                <h3> City </h3>
-                <TextFieldValidated
-                    value={this.state.value}
-                    Error={this.state.value === 'City' ? null : 'Please enter a City in the following format:'}
-                />
-                <h3> State </h3>
-                <TextFieldValidated
-                    value={this.state.value}
-                    Error={this.state.value === 'State' ? null : 'Please enter a State in the following format:'}
-                />
-                <h3> Zip </h3>
-                <TextFieldValidated
-                    value={this.state.value}
-                    Error={this.state.value === 'Zip' ? null : 'Please enter a Zip Code in the following format:'}
+                <Form
+                    schema={schema}
+                    uiSchema={uiSchema}
                 />
             </div>
         );
     }
-});
+}
 
 export default AddressForm;
--- src/components/TimeSelector.js
+++ src/components/TimeSelector.js
@@ -2,9 +2,10 @@
 //use moment library for date formatting, and sharing data with the table
 import React, {Component} from "react";
 import moment from 'moment';
-import {DateUtils} from 'react-day-picker';
-import {SingleSelect, DateSelect, Button} from "lucid-ui";
-import '../../node_modules/lucid-ui/dist/index.css';
+import DayPicker, {DateUtils} from 'react-day-picker';
+import {SingleSelect, Button} from "lucid-ui";
+import 'react-day-picker/lib/style.css';
+import '../styles/css/bootstrap.min.css';
 
 const { Placeholder, Option } = SingleSelect;
 
@@ -13,58 +14,56 @@ const { Placeholder, Option } = SingleSelect;
 * This uses the Moment library for date tools and calculations.
 *
 */
-
 const currDay = new Date(moment());
 const lastWeek = new Date(moment().subtract(7, 'days'));
 const twoWeeks = new Date(moment().add(14, 'days'))
 
-//console.log(DateUtils.isDayBefore(, twoWeeks))
-
-const TimeSelector = React.createClass ({
-    getInitialState() {
-		return {
-            selectMode: 'from',
+class TimeSelector extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
             from: lastWeek,
             to: currDay,
-		};
-	},
+        };
+        this.handleSelectDate = this.handleSelectDate.bind(this);
+        this.handleReset = this.handleReset.bind(this);
+    }
 
-    handleSelectDate(date) {
-		const { selectMode } = this.state;
-		this.setState({
-			selectMode: 'to',
-			[selectMode]: date,
-		});
+    handleSelectDate = date => {
+        const range = DateUtils.addDayToRange(date, this.state)
+		this.setState(range)
         //if the range is greater than 2 weeks, throw Error
         if (DateUtils.isDayAfter(date, twoWeeks)){
             console.log("WE have a bad date range!");
         }
-	},
-    handleReset() {
+	}
+
+    handleReset = e => {
+        e.preventDefault
 		this.setState({
-			selectMode: 'from',
 			from: null,
 			to: null,
 		});
-	},
+	}
+
     render() {
-        const { selectMode, from, to } = this.state;
+        const { from, to } = this.state;
         return (
-            <section style={{maxWidth: 400, height: 200}}>
-                <DateSelect
-                    from={from}
-                    to={to}
-                    selectMode={this.selectMode}
-                    onSelectDate={this.handleSelectDate}
+            <div className="RangeExample">
+                <DayPicker
+                    numberOfMonths={2}
+                    selectedDays={[from, {from, to}]}
+                    onDayClick={this.handleSelectDate}
+                    fixedWeeks
                 />
                 <div>
                     From: {from && from.toLocaleDateString('en-US')},
                     To: {to && to.toLocaleDateString('en-US')}
                 </div>
-                <Button onClick={this.handleReset}>Reset</Button>
-            </section>
+                <button onClick={this.handleReset}>Reset</button>
+            </div>
         );
     }
-});
+}
 
 export default TimeSelector;
